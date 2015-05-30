a29a009a6a285972d5f610e643f5eb66cb74b296
mike <mikefc@planckenergy.com>
2015-05-30 20:36:46 +1000

    documented data files using roxygen

--- R/au.population.change.R
+++ R/au.population.change.R
@@ -1,8 +1,8 @@
 #' All population data for Australia
 #'
-#' All population data for Australia from 1788-2010 by state and nationally.
+#' All population change data for Australia from 1971-2011 by state and nationally - including births, deaths and international and interstate migration.
 #'
-#' @source ABS 3105.0.65.0012014 Table 1.2
+#' @source ABS 3105.0.65.0012014 Table 1.3
 #' @format Data frame with columns
 #' \describe{
 #'  \item{location}{State abbreviation or 'Australia'}
@@ -11,5 +11,5 @@
 #'  \item{sex}{Male, Female, Persons (all data)}
 #' }
 #' @examples
-#'      au.population
-"au.population"
+#'      au.population.change
+"au.population.change"
--- R/au.population.recent.R
+++ R/au.population.recent.R
@@ -1,8 +1,8 @@
 #' All population data for Australia
 #'
-#' All population data for Australia from 1788-2010 by state and nationally.
+#' All population data for Australia from 1901-2012 by state and nationally.
 #'
-#' @source ABS 3105.0.65.0012014 Table 1.2
+#' @source ABS 3105.0.65.0012014 Table 1.3
 #' @format Data frame with columns
 #' \describe{
 #'  \item{location}{State abbreviation or 'Australia'}
@@ -11,5 +11,5 @@
 #'  \item{sex}{Male, Female, Persons (all data)}
 #' }
 #' @examples
-#'      au.population
-"au.population"
+#'      au.population.recent
+"au.population.recent"
